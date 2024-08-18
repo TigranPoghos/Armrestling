bf4fd73387fce5c13cc145051d29894d1e5d6f0d
TigranPoghos <tigran2002poghosyan@gmail.com>
2024-08-18 21:04:29 +0300

    correct

--- script/script.js
+++ script/script.js
@@ -1,30 +1,37 @@
 document.addEventListener("DOMContentLoaded", function(){
 
     //галерея
-    var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 3,
-        spaceBetween: 20,
-        loop: true,
-        autoplay: {
-            delay: 0,
-            disableOnInteraction: false,
-        },
-        speed: 3000,
-    });
+    // var swiper = new Swiper(".mySwiper", {
+    //     slidesPerView: 3,
+    //     spaceBetween: 20,
+    //     loop: true,
+    //     autoplay: {
+    //         delay: 0,
+    //         disableOnInteraction: false,
+    //     },
+    //     speed: 3000,
+    // });
 
 
-    var swiper2 = new Swiper(".mySwiper2", {
-        slidesPerView: 3,
-        spaceBetween: 20,
-        loop: true,
-        autoplay: {
-            delay: 0,
-            disableOnInteraction: false,
-            reverseDirection: true
-        },
-        speed: 3000,
-    });
+    // var swiper2 = new Swiper(".mySwiper2", {
+    //     slidesPerView: 3,
+    //     spaceBetween: 20,
+    //     loop: true,
+    //     autoplay: {
+    //         delay: 0,
+    //         disableOnInteraction: false,
+    //         reverseDirection: true,
+    //     },
+    //     speed: 3000,
+    // });
     
+    const marquee = document.querySelector('.marquee');
+    const containerWidth = document.querySelector('.marquee-container').offsetWidth;
+    const marqueeWidth = marquee.scrollWidth;
+
+    const animationDuration = (marqueeWidth / 500) + 's';
+
+    marquee.style.animationDuration = animationDuration;
 
 
 })
