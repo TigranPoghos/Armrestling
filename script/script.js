352431895927a1ce0e8f89c8455b82e3c6ae5478
TigranPoghos <tigran2002poghosyan@gmail.com>
2024-08-21 20:19:49 +0300

    event

--- script/script.js
+++ script/script.js
@@ -13,7 +13,6 @@ document.addEventListener("DOMContentLoaded", function(){
     const marquee2 = document.querySelector('.marquee2');
     const containerWidth2 = document.querySelector('.marquee-container2').offsetWidth;
     const marqueeWidth2 = marquee2.scrollWidth;
-
     const animationDuration2 = (marqueeWidth2 / 100) + 's';
 
     marquee2.style.animationDuration = animationDuration2;
@@ -22,16 +21,13 @@ document.addEventListener("DOMContentLoaded", function(){
 
     const preloader = document.querySelector('.preloader');
 
-    // Функция, вызываемая по завершении всех анимаций
     function hidePreloader() {
         preloader.classList.add('preloader--hidden');
-        // Дополнительно удаляем элемент из DOM после завершения перехода
         setTimeout(() => {
-            preloader.style.display = 'none'; // Или можете удалить элемент полностью
-        }, 3000); // Задержка должна совпадать с длительностью перехода
+            preloader.style.display = 'none';
+            document.body.style.overflow = 'auto';
+        }, 3000);
     }
-
-    // Добавляем обработчик события окончания анимации
     preloader.addEventListener('animationend', hidePreloader);
     
 
