913479a41c7e0e0a11c5b6378b37854dddd88b0e
TigranPoghos <tigran2002poghosyan@gmail.com>
2024-08-21 15:56:41 +0300

    preloader

--- script/script.js
+++ script/script.js
@@ -18,4 +18,32 @@ document.addEventListener("DOMContentLoaded", function(){
 
     marquee2.style.animationDuration = animationDuration2;
 
+
+
+
+
+    // window.onload = function () {
+    //     document.body.classList.add('loaded_hiding');
+    //     window.setTimeout(function () {
+    //       document.body.classList.add('loaded');
+    //       document.body.classList.remove('loaded_hiding');
+    //     }, 500);
+    // }
+
+    // window.addEventListener('load', () => {
+    //     setTimeout(() => {
+    //         const preloader = document.getElementById('.preloader');
+    //         preloader.classList.add('hidden');
+            
+    //         // Показываем контент, когда прелоадер скрыт
+    //         const header = document.getElementById('.header');
+    //         const main = document.getElementById('.main');
+    //         const footer = document.getElementById('.footer');
+
+    //         header.style.display = 'flex';
+    //         main.style.display = 'flex';
+    //         footer.style.display = 'flex';
+    //     }, 3000); // Задержка 3 секунды
+    // });
+
 })
