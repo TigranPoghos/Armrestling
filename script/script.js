0200b765934cedda40194307d64d4aba9251f45c
TigranPoghos <tigran2002poghosyan@gmail.com>
2025-03-05 14:41:26 +0300

    js correct

--- script/script.js
+++ script/script.js
@@ -224,50 +224,56 @@ document.addEventListener("DOMContentLoaded", function(){
     const bodyIndex = document.querySelector('.body__index');
     const tl = gsap.timeline()
 
-    tl.to('.preloader__line-top', {
-        height: 50,
-        duration: 1,
-    },0).to('.preloader__line-middle', {
-        height: 86,
-        duration: 1,
-    },0).to('.preloader__line-bottom', {
-        height: 115,
-        duration: 1,
-    },0) .to('.preloader__line-top', {
-        height: 115,
-        duration: 1,
-    },1).to('.preloader__line-middle', {
-        height: 50,
-        duration: 1,
-    },1).to('.preloader__line-bottom', {
-        height: 115,
-        duration: 1,
-    },1).to('.preloader__line-top', {
-        height: 115,
-        duration: 1,
-    },2).to('.preloader__line-middle', {
-        height: 86,
-        duration: 1,
-    },2).to('.preloader__line-bottom', {
-        height: 50,
-        duration: 1,
-    },2).to('.preloader__line-top', {
-        height: 86,
-        duration: 1,
-    },3).to('.preloader__line-middle', {
-        height: 115,
-        duration: 1,
-    },3).to('.preloader__line-bottom', {
-        height: 86,
-        duration: 1,
-    },3).then(() => {
-        preloader?.classList.add('hidden');
-        setTimeout(() => {
-            preloader.style.display = 'none';
-            bodyIndex?.classList.add('active');
-            startAnimations();
-        }, 200);
-    });    
+    const PreloaderLineTop = document.querySelector('.preloader__line-top')
+    const PreloaderLineMiddle = document.querySelector('.preloader__line-middle')
+    const PreloaderLineBottom = document.querySelector('.preloader__line-bottom')
+
+    if ((PreloaderLineTop) && (PreloaderLineMiddle) && (PreloaderLineBottom)) {
+        tl.to('.preloader__line-top', {
+            height: 50,
+            duration: 1,
+        },0).to('.preloader__line-middle', {
+            height: 86,
+            duration: 1,
+        },0).to('.preloader__line-bottom', {
+            height: 115,
+            duration: 1,
+        },0) .to('.preloader__line-top', {
+            height: 115,
+            duration: 1,
+        },1).to('.preloader__line-middle', {
+            height: 50,
+            duration: 1,
+        },1).to('.preloader__line-bottom', {
+            height: 115,
+            duration: 1,
+        },1).to('.preloader__line-top', {
+            height: 115,
+            duration: 1,
+        },2).to('.preloader__line-middle', {
+            height: 86,
+            duration: 1,
+        },2).to('.preloader__line-bottom', {
+            height: 50,
+            duration: 1,
+        },2).to('.preloader__line-top', {
+            height: 86,
+            duration: 1,
+        },3).to('.preloader__line-middle', {
+            height: 115,
+            duration: 1,
+        },3).to('.preloader__line-bottom', {
+            height: 86,
+            duration: 1,
+        },3).then(() => {
+            preloader?.classList.add('hidden');
+            setTimeout(() => {
+                preloader.style.display = 'none';
+                bodyIndex?.classList.add('active');
+                startAnimations();
+            }, 200);
+        });       
+    }
 
     if (!sessionStorage.getItem('preloaderShown')) {
         preloader.style.display = 'block';
